ca0879f6de9db1bac4fa78ce249607dc24617c35
xushuwei <xushuwei@youku.com>
2016-02-16 19:14:47 +0800

    fix bug

--- funpages/face/scripts/scripts.js
+++ funpages/face/scripts/scripts.js
@@ -11,22 +11,22 @@ function html5glasses() {
 
 	ctx.drawImage(video, 0, 0, video.width, video.height, 0, 0, canvas.width, canvas.height);
 
-	var comp = ccv.detect_objects({ "canvas" : (ccv.pre(canvas)),
-									"cascade" : cascade,
-									"interval" : 5,
-									"min_neighbors" : 1 });
+	// var comp = ccv.detect_objects({ "canvas" : (ccv.pre(canvas)),
+	// 								"cascade" : cascade,
+	// 								"interval" : 5,
+	// 								"min_neighbors" : 1 });
 
-	time_dump.innerHTML = "Process time : " + ((new Date()).getTime() - elapsed_time).toString() + "ms";
+	// time_dump.innerHTML = "Process time : " + ((new Date()).getTime() - elapsed_time).toString() + "ms";
 
-	for (var i = 0; i < comp.length; i++) {
-		ctx.drawImage(glasses, comp[i].x, comp[i].y,comp[i].width, comp[i].height);
-	}
+	// for (var i = 0; i < comp.length; i++) {
+	// 	ctx.drawImage(glasses, comp[i].x, comp[i].y,comp[i].width, comp[i].height);
+	// }
 }
 
 
 
 video.addEventListener('play', function() {
-	vidInterval = setInterval(html5glasses,0); 
+	//vidInterval = setInterval(html5glasses,0); 
 });
 
 video.addEventListener('ended', function() {
