35a9da7b3ecc52bd2ce360ad0782062f78a96441
xushuwei <xushuwei@youku.com>
2016-02-16 17:03:20 +0800

    fix bug

--- funpages/face/scripts/scripts.js
+++ funpages/face/scripts/scripts.js
@@ -31,7 +31,7 @@ function html5glasses() {
 /* Events */ 
 
 video.addEventListener('play', function() {
-	vidInterval = setInterval(html5glasses,50); 
+	vidInterval = setInterval(html5glasses,0); 
 });
 
 video.addEventListener('ended', function() {
